f22ff4447d136ac3a64c06c2c5a9562f892f5d9f
Geoffrey Braaf <geoffrey@braaf.nl>
2016-03-16 12:29:41 +0100

    added some comments

--- src/I18NDotNet.Address/AddressFieldKey.cs
+++ src/I18NDotNet.Address/AddressFieldKey.cs
@@ -2,16 +2,54 @@ namespace I18N.Address
 {
 	public enum AddressFieldKey
 	{
-		N, // Name
-		O, // Organisation
-		A, // Street Address Line(s)
-		D, // Dependent locality (may be an inner-city district or a suburb)
-		C, // City or Locality
-		S, // Administrative area such as a state, province, island etc
-		Z, // Zip or postal code
-		X, // Sorting code
+		/// <summary>
+		/// Name
+		/// </summary>
+		N,
 
+		/// <summary>
+		/// Organisation
+		/// </summary>
+		O,
+
+		/// <summary>
+		/// Street Address Line(s) 
+		/// </summary>
+		A,
+
+		/// <summary>
+		/// Dependent locality (may be an inner-city district or a suburb)
+		/// </summary>
+		D,
+
+		/// <summary>
+		/// City or Locality
+		/// </summary>
+		C,
+
+		/// <summary>
+		/// Administrative area such as a state, province, island etc
+		/// </summary>
+		S,
+
+		/// <summary>
+		/// Zip or postal code
+		/// </summary>
+		Z,
+
+		/// <summary>
+		/// Sorting code
+		/// </summary>
+		X,
+
+		/// <summary>
+		/// Country (never part of the data received from google, as it is part of the key)
+		/// </summary>
 		Country,
+
+		/// <summary>
+		/// Optionl language of the address, used for region matching (never part of the data received from google, as it is part of the key)
+		/// </summary>
 		Language
 	}
 }
